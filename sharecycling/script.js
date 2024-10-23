93694164065bb7277cc9aa506e370e0aa9aba530
mayasato0401 <maya.s0401.wdr@gmail.com>
2024-10-24 00:11:19 +0900

    WordPressではない

--- sharecycling/script.js
+++ sharecycling/script.js
@@ -1,13 +1,19 @@
-// jQuery(function($) {
+$(function() {
 
-  jQuery('.sp-nemu').on('click', function(){
-    jQuery('.sp-nemu-open').fadeIn(200);
-    jQuery('.sp-nemu-open').css('opacity', '1');
-    });
-
-  jQuery('.sp-nemu-close').on('click', function(){
-    jQuery('.sp-nemu-open').fadeOut(200);
-    jQuery('.sp-nemu-open').css('opacity', '0');
+  $('.sp-nemu').on('click', function(){
+      $('.sp-nemu-open').fadeIn(200);
+      $('.sp-nemu-open').css('opacity', '1');
   });
 
-// });
+  $('.sp-nemu-close').on('click', function(){
+    $('.sp-nemu-open').fadeOut(200);
+    $('.sp-nemu-open').css('opacity', '0');
+  });
+
+  $('.price, .cyclestation').on('inview', function(event, isInView){
+    if(isInView){
+      $('.price, .cyclestation').stop().addClass("show");
+    }
+  });
+
+});
